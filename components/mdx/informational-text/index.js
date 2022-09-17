6c1a55330b4527ced1a3806317090e831b8f9434
dovydinho <dovydas.lapinskas@tutanota.com>
2022-09-17 22:51:54 +0300

    Small screen ui update

--- components/mdx/informational-text/index.js
+++ components/mdx/informational-text/index.js
@@ -1,13 +1,11 @@
-import { InformationCircleIcon } from "@heroicons/react/outline";
-import cn from "classnames";
+import { InformationCircleIcon } from '@heroicons/react/outline';
+import cn from 'classnames';
 
 export default function InformationalText({ children }) {
   return (
     <>
-      <div className="flex w-full p-4 rounded-lg my-4 bg-indigo-100 dark:bg-indigo-900 text-indigo-900 dark:text-indigo-100 overflow-auto">
-        <span>
-          <InformationCircleIcon className="w-6 h-6 mr-2 inline-block align-middle" />
-        </span>
+      <div className="flex flex-wrap w-full p-4 rounded-lg my-4 bg-indigo-100 dark:bg-indigo-900 text-indigo-900 dark:text-indigo-100 overflow-auto">
+        <InformationCircleIcon className="my-auto w-6 h-6 mr-2 inline-block align-middle" />
         {children}
       </div>
     </>

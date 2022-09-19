0b06e9a0c5e4724f735bd55f10373edb1b7c401b
dovydinho <dovydas.lapinskas@tutanota.com>
2022-09-19 17:15:29 +0300

    Image preview UI update

--- components/mdx/image-caption/index.js
+++ components/mdx/image-caption/index.js
@@ -1,7 +1,6 @@
 import Image from 'next/image';
 import cn from 'classnames';
 import { useEffect, useState } from 'react';
-import { XCircleIcon } from '@heroicons/react/outline';
 
 export default function ImageCaption({ url, width, height, caption }) {
   const [popupImage, setPopupImage] = useState(false);
@@ -52,24 +51,16 @@ export default function ImageCaption({ url, width, height, caption }) {
         }`}
       >
         <div className="m-auto">
-          <a
-            onClick={() => setPopupImage(false)}
-            className="absolute z-10 left-1/2 -mt-5 rounded-full w-12 h-12 bg-gray-100 dark:bg-gray-900"
-          >
-            <XCircleIcon className="w-12 h-12 text-pink-500/50  hover:text-pink-500 hover:scale-125 duration-150" />
-          </a>
-          <a className="relative" onClick={() => setPopupImage(false)}>
-            <Image
-              src={url}
-              alt={caption}
-              width={1.5 * width}
-              height={1.5 * height}
-              placeholder="blur"
-              blurDataURL="/img/frontpage.png"
-              className={cn('rounded-xl')}
-              quality="100"
-            />
-          </a>
+          <Image
+            src={url}
+            alt={caption}
+            width={1.5 * width}
+            height={1.5 * height}
+            placeholder="blur"
+            blurDataURL="/img/frontpage.png"
+            className={cn('rounded-xl')}
+            quality="100"
+          />
         </div>
       </div>
     </>

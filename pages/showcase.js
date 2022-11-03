a2ee9b5b649dc0710b0350be7c8a34104abae324
dovydinho <dovydas.lapinskas@tutanota.com>
2022-11-03 09:02:56 +0200

    Thumbnail file type update

--- pages/showcase.js
+++ pages/showcase.js
@@ -25,7 +25,7 @@ export default function Showcase() {
           <ProjectCard
             title="Decentralized Exchange"
             slug="https://dex.dovydas.io"
-            image="/img/blog/my-web3-app-overview-dex/dex-1.png"
+            image="/img/blog/my-web3-app-overview-dex/dex-1.jpg"
             tags={['DEFI', 'Ethereum', 'Next.js', 'ApexCharts']}
             description="DEX allow users to trade ERC-20 tokens listed on the exchange. It is deployed on Ethereum Goerli Test Network and uses USDC stablecoin testnet representation for trade settlements."
             network="Ethereum (Goerli Test Network)"
@@ -35,7 +35,7 @@ export default function Showcase() {
           <ProjectCard
             title="Treasure Hunt Outdoors"
             slug="https://treasure-hunt-outdoors.dovydas.io"
-            image="/img/blog/my-web3-app-overview-treasure-hunt-outdoors/treasure-hunt-outdoors-7.png"
+            image="/img/blog/my-web3-app-overview-treasure-hunt-outdoors/treasure-hunt-outdoors-7.jpg"
             tags={['Web3', 'Ethereum', 'Next.js', 'Leaflet']}
             description="Web3 application powered by Ethereum and inspired by adventure game Geocaching, 
             where community can look for caches pinned on the game map or create new challenges for other adventure enthusiasts."

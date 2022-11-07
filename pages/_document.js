fd8bc06f48c5a137b89cc961c0109137286efc97
dovydinho <dovydas.lapinskas@tutanota.com>
2022-11-07 15:32:36 +0200

    Google Analytics added

--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,25 @@
-import { Html, Main, Head, NextScript } from "next/document";
+import { Html, Main, Head, NextScript } from 'next/document';
 
 export default function Document(props) {
   return (
     <Html lang="en">
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+          });
+        `
+        }}
+      />
       <Head />
       <body className="text-gray-800 dark:text-gray-200 font-light">
         <Main />
